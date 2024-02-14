7894881883836a4ed3aa2c6afc1a53e1c464b591
neerrrajj <neerrrajj@outlook.com>
2024-02-14 06:38:30 +0530

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    do {
+        let m = Math.floor(low + (high - low) / 2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] > needle) {
+            high = m;
+        } else {
+            low = m + 1;
+        }
+    } while (low < high);
+    return false;
 }
